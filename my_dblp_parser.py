047ea8fdaae0c4a213037585ce70b2c69c6a39db
leonidk <lkeselman@gmail.com>
2019-05-19 17:08:32 -0400

    add ipcai merging

--- my_dblp_parser.py
+++ my_dblp_parser.py
@@ -442,7 +442,13 @@ for event, elem in parser:
                 (vol, num) = TVCG_VR_Volume[year]
                 if (volume == str(vol)) and (number == str(num)):
                     venue = 'VR'
-        
+        elif venue == 'Int. J. Computer Assisted Radiology and Surgery':
+            ipcai_lookup = {2015: [6], 2016: [6], 2017: [6,7], 2018:[5,6], 2019:[6,7]}
+            number = int(number)
+            print(volume,number,year,title,authors)
+            if (year in ipcai_lookup) and (number in ipcai_lookup[year]):
+                venue = 'IPCAI'
+
         # Special handling for ISMB.
         if venue == 'Bioinformatics':
             if year in ISMB_Bioinformatics:
