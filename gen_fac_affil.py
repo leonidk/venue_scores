09e1cd6e7d32194a4a766ece52c845dcaddbf3a0
leonidk <lkeselman@gmail.com>
2019-09-19 16:17:50 -0400

    added place to add extra people for faculty computation/regression

--- gen_fac_affil.py
+++ gen_fac_affil.py
@@ -3,15 +3,21 @@ with open('csrankings.csv', mode='r') as infile:
     reader = csv.DictReader(infile)
     with open('industry.csv', mode='r') as infile2:
         reader2 = csv.DictReader(infile2)
-        with open('faculty-affiliations.csv', 'w') as facultyaffs:
-            facfieldnames = ['name', 'affiliation']
-            facWriter = csv.DictWriter(facultyaffs, fieldnames=facfieldnames)
-            facWriter.writeheader()
-            for row in reader:
-                f = { 'name' : row['name'].strip().rstrip(),
-                        'affiliation' : row['affiliation'].strip().rstrip() }
-                facWriter.writerow(f)
-            for row in reader2:
-                f = { 'name' : row['name'].strip().rstrip(),
-                        'affiliation' : row['affiliation'].strip().rstrip() }
-                facWriter.writerow(f)
+        with open('my_extra.csv', mode='r') as infile3:
+            reader3 = csv.DictReader(infile3)
+            with open('faculty-affiliations.csv', 'w') as facultyaffs:
+                facfieldnames = ['name', 'affiliation']
+                facWriter = csv.DictWriter(facultyaffs, fieldnames=facfieldnames)
+                facWriter.writeheader()
+                for row in reader:
+                    f = { 'name' : row['name'].strip().rstrip(),
+                            'affiliation' : row['affiliation'].strip().rstrip() }
+                    facWriter.writerow(f)
+                for row in reader2:
+                    f = { 'name' : row['name'].strip().rstrip(),
+                            'affiliation' : row['affiliation'].strip().rstrip() }
+                    facWriter.writerow(f)
+                for row in reader3:
+                    f = { 'name' : row['name'].strip().rstrip(),
+                            'affiliation' : row['affiliation'].strip().rstrip() }
+                    facWriter.writerow(f)
